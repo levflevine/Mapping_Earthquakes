74a84077363341f1b7824fe5241c94eb806b99ee
levflevine <levflevine@gmail.com>
2021-12-07 18:43:12 -0500

    single point

--- Mapping_Single_Points/static/js/config.js
+++ Mapping_Single_Points/static/js/config.js
@@ -1,1 +1,1 @@
-const API_KEY = 'pk.eyJ1IjoibGV2ZmxldmluZSIsImEiOiJja3d1MXZpZjkxbWd1MnZvODN6Z2pqaDl0In0.ntAtXj9tyo9PjrKvLrrnBQ';
+API_KEY = 'pk.eyJ1IjoibGV2ZmxldmluZSIsImEiOiJja3d1MXZpZjkxbWd1MnZvODN6Z2pqaDl0In0.ntAtXj9tyo9PjrKvLrrnBQ'
