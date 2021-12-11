6bcb772c88458c44b7d8eb83d7de9fc593206402
levflevine <levflevine@gmail.com>
2021-12-11 08:09:54 -0500

    13.6.1

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,138 @@
+// Create the map object with center and zoom level.
+//let map = L.map('mapid').setView([30, 30], 2);
+
+// Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection",
+// "features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
+
+  // Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     onEachFeature: function (feature, layer) {
+//       console.log(layer);
+//       layer.bindPopup(feature.properties.name);}})
+//         .addTo(map);
+
+
+
+  // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     color: "yellow"
+//  }).addTo(map);
+
+// Get data from cities.js
+// let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {radius: city.population/100000,
+//         color: 'orange',
+//         fillcolor: '#ffffa1'
+    
+    
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>").addTo(map);
+// });
+  
+// L.circle([34.0522, -118.2437], {
+//     radius: 100, 
+//     color: 'orange',
+//     fillcolor: '#ffffa1'
+//  }).addTo(map);
+
+//  L.circleMarker([34.0522, -118.2437], {radius:10, color:'black', fillcolor: '#ffffa1'}).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "satelliteStreets": satelliteStreets
+  };
+
+  // Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
+// Accessing the airport GeoJSON URL
+//let airportData = "https://raw.githubusercontent.com/levflevine/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+
+// Accessing the Toronto airline routes GeoJSON URL.
+//let torontoData = "https://raw.githubusercontent.com/levflevine/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+//let torontoHoods = "https://raw.githubusercontent.com/levflevine/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
+
+//Create a style for the lines.
+let myStyle = {
+  color: "blue",
+  weight: 1, 
+  fillColor:'yellow',
+  opacity: 0.5
+};
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, 
+        {style: myStyle,
+        onEachFeature: function (feature, layer) {
+        console.log(layer);
+        layer.bindPopup("Neighborhood: " + feature.properties.AREA_NAME);}}
+        )
+          
+    .addTo(map);
+});
